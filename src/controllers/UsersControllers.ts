2d6cbcab12e7837065bede307c5d1892ac9d43a2
edvan silva <edvansilvapereira@gmail.com>
2023-05-30 16:20:47 -0300

    criando routes

---
+++ src/controllers/UsersControllers.ts
@@ -0,0 +1,17 @@
+
+class UsersController{
+    index(){
+//buscar todos
+    }
+    show(){
+     //buscar somente um   
+    }
+    store(){
+        //criar
+    }
+    auth(){
+        //autenticação
+    }
+}
+
+export {UsersController};
